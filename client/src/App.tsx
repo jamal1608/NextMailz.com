3cc8290f243dd92d3dec567ca5dd7aa9673d7db4
Jamal Ahmed <ahmedbhatti909@gmail.com>
2025-11-11 22:51:07 -0600

    Add blog pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,14 +4,17 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
-import { Route, Switch } from "wouter";
+import { Route, Switch, useRoute } from "wouter";
 import Home from "@client/pages/Home";
 import About from "@client/pages/About";
 import Privacy from "@client/pages/Privacy";
 import NotFound from "@client/pages/not-found";
-import Blog from "@client/pages/blog/index";
-import BlogPost from "@client/pages/blog/BlogPost";
 import "./index.css";
+
+// Lazy load blog components
+import { lazy, Suspense } from "react";
+const Blog = lazy(() => import("@client/pages/blog/index"));
+const BlogPost = lazy(() => import("@client/pages/blog/BlogPost"));
 
 function App() {
   return (
@@ -21,14 +24,16 @@ function App() {
           <Navigation />
           
           <main className="flex-1">
-            <Switch>
-              <Route path="/" component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/blog" component={Blog} />
-              <Route path="/blog/:id" component={BlogPost} />
-              <Route path="/privacy" component={Privacy} />
-              <Route component={NotFound} />
-            </Switch>
+            <Suspense fallback={<div className="flex items-center justify-center h-64">Loading...</div>}>
+              <Switch>
+                <Route path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/blog" component={Blog} />
+                <Route path="/blog/:id" component={BlogPost} />
+                <Route path="/privacy" component={Privacy} />
+                <Route component={NotFound} />
+              </Switch>
+            </Suspense>
           </main>
           
           <Footer />
